93f57ceae55b91694a9d27a95cdc2b3523412393
ss-41 <shamir.ssesook@gmail.com>
2026-07-30 19:12:25 -0400

    added test case

--- Hello/src/test/HelloTest.java
+++ Hello/src/test/HelloTest.java
@@ -1,8 +1,15 @@
 package test;
 
+import static org.junit.Assert.*;
+
+import org.junit.*;
+
+import hello.*;
+
 public class HelloTest {
-    // @Test 
-    // public void test1(){
-    //     Hello hello = new Hello();
-    // }
+    @Test 
+    public void test1(){
+        Hello hello = new Hello();
+        assertEquals("Ahhhh!", hello.talk());
+    }
 }
