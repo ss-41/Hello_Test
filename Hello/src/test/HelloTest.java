312e04ad283e82b88faa345b9cb444c64b6406d2
ss-41 <shamir.ssesook@gmail.com>
2026-07-31 00:45:50 -0400

    added test case

--- Hello/src/test/HelloTest.java
+++ Hello/src/test/HelloTest.java
@@ -10,6 +10,13 @@ public class HelloTest {
     @Test 
     public void test1(){
         Hello hello = new Hello();
-        assertEquals("Ahhhh!", hello.talk());
+        assertTrue(hello.talk().contains("Ahhhh"));
     }
+
+    @Test 
+    public void test2(){
+        Hello hello = new Hello();
+        assertTrue(hello.talk().equals("Ahhhh!"));
+    }
+    
 }
